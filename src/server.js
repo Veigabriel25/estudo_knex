1d5933c44860af3175f721906424eb18da215dd1
Veigabriel24 <gabrielveig@outlook.com>
2020-09-09 19:01:12 -0300

    commit 2

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
-
-
+const knex = require('./database');
+const routes = require('./routes');
 const app = express();
+
+app.use(routes)
+
+app.listen(3333);
 
 /*git commit -m "first commit"
 git branch -M master
