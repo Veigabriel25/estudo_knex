208e2185dc4722b8b4f75290b7fbdf14a0f2ae63
Veigabriel24 <gabrielveig@outlook.com>
2020-09-09 21:59:48 -0300

    crud

--- src/server.js
+++ src/server.js
@@ -3,7 +3,25 @@ const knex = require('./database');
 const routes = require('./routes');
 const app = express();
 
-app.use(routes)
+app.use(express.json());
+app.use(routes);
+
+
+//not found
+
+
+app.use((req, res, next) => {
+  const error = new Error("not found");
+  error.status = 404;
+  next(error);
+})
+
+//catchall
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({ error: error.message })
+});
 
 app.listen(3333);
 
